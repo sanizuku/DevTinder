a309484fe70bf532f1bb31adec2a2688d76e3f06
sandeep <sandeeps16xgmail.com>
2024-12-11 16:08:39 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.use(
-  "/admin",
-  (req, res, next) => {
-    console.log("Admin auth is getting checked");
-    const token = "xyzz";
-    const isAdminAuthorized = token === "xyz";
-    if (!isAdminAuthorized) {
-      res.status(401).send("unauthorized request");
-    } else {
-      next();
-    }
-  },
-  (req, res) => {
-    res.send("admin authenticated");
+
+app.get("/getUserData", (req, res) => {
+  try {
+    //logic of db call and get userData
+    throw new Error("asls");
+    res.send("user Data Send");
+  } catch (err) {
+    res.status(500).send("Something went wrong");
   }
-);
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    console.log("Error");
+    res.status(500).send("something went wrong2");
+  }
+});
 
 app.listen(7777, () => {
   console.log("server is successfully listening on port 7777...");
